5d3ca55778e0d8bce9df133702686aedc7ef7e9a
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2025-01-23 21:47:03 +0200

    update carousel

--- src/components/Sections/Testimonials/Testimonials.tsx
+++ src/components/Sections/Testimonials/Testimonials.tsx
@@ -1,9 +1,10 @@
 "use client";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/swiper-bundle.css";
 import Image from "next/image";
 import { Fragment, useState } from "react";
-import useEmblaCarousel from "embla-carousel-react";
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+// import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
 import ProgressBar from "../../UI/ProgressBar";
 import Container from "../../Container";
@@ -15,13 +16,6 @@ import css from "./Testimonials.module.scss";
 const MAX_LENGTH = 150;
 
 const Testimonials = () => {
-  const [emblaRef, emblaApi] = useEmblaCarousel({
-    loop: true,
-  });
-
-  const nextSlide = () => emblaApi?.scrollNext();
-  const prevSlide = () => emblaApi?.scrollPrev();
-
   const [isModalOpen, setModalOpen] = useState<boolean>(false);
   const [fullText, setFullText] = useState<string>("");
 
@@ -45,60 +39,68 @@ const Testimonials = () => {
       </h2>
 
       <Container>
-        <div className={css.embla} ref={emblaRef}>
-          <div className={css.embla__container}>
-            {testimonials.map((testimonial, index) => (
-              <div className={css.embla__slide} key={index}>
-                <div>
-                  <div className={css.testimonialHeader}>
-                    <Image
-                      className={css.testimonialPhoto}
-                      src={testimonial.photo}
-                      alt={testimonial.name}
-                      width="100"
-                      height="100"
-                    />
-                    <p className={css.testimonialName}>{testimonial.name}</p>
-                  </div>
+        <div className={css.swiperWrapper}>
+          <Swiper
+            modules={[Pagination]}
+            pagination={{ clickable: true }}
+            spaceBetween={20}
+            slidesPerView={1}
+            breakpoints={{
+              768: { slidesPerView: 2, spaceBetween: 30 },
+              960: { slidesPerView: 3, spaceBetween: 8 },
+              1200: { slidesPerView: 3, spaceBetween: 30 },
+              1440: { slidesPerView: 3, spaceBetween: 30 },
 
-                  <div className={css.testimonialTextWrapper}>
-                    <div className={css.testimonialText}>
-                      {testimonial.text.length > MAX_LENGTH ? (
-                        <>
-                          {splitText(testimonial.text.slice(0, MAX_LENGTH))}
-                          <span className={css.ellipsis}>...</span>
-                        </>
-                      ) : (
-                        splitText(testimonial.text)
+            }}
+            loop={true}
+            className={css.customSwiper}
+          >
+            {testimonials.map((testimonial, index) => (
+              <SwiperSlide key={index}>
+                <div className={css.slide}>
+                  <div>
+                    <div className={css.testimonialHeader}>
+                      <Image
+                        className={css.testimonialPhoto}
+                        src={testimonial.photo}
+                        alt={testimonial.name}
+                        width="100"
+                        height="100"
+                      />
+                      <p className={css.testimonialName}>{testimonial.name}</p>
+                    </div>
+
+                    <div className={css.testimonialTextWrapper}>
+                      <div className={css.testimonialText}>
+                        {testimonial.text.length > MAX_LENGTH ? (
+                          <>
+                            {splitText(testimonial.text.slice(0, MAX_LENGTH))}
+                            <span className={css.ellipsis}>...</span>
+                          </>
+                        ) : (
+                          splitText(testimonial.text)
+                        )}
+                      </div>
+                      {testimonial.text.length > MAX_LENGTH && (
+                        <button
+                          className={css.readMoreButton}
+                          onClick={() => openModal(testimonial.text)}
+                        >
+                          Читати далі
+                        </button>
                       )}
                     </div>
-                    {testimonial.text.length > MAX_LENGTH && (
-                      <button
-                        className={css.readMoreButton}
-                        onClick={() => openModal(testimonial.text)}
-                      >
-                        Читати далі
-                      </button>
-                    )}
                   </div>
+                  <ProgressBar
+                    initialLevel={testimonial.initialLevel || 0}
+                    currentLevel={testimonial.currentLevel || 0}
+                  />
                 </div>
-                <ProgressBar
-                  initialLevel={testimonial.initialLevel || 0}
-                  currentLevel={testimonial.currentLevel || 0}
-                />
-              </div>
+              </SwiperSlide>
             ))}
-          </div>
-          <button className={css.prevButton} onClick={prevSlide}>
-            {/* &#8592; */}
-            <FaArrowLeft />
-          </button>
-          <button className={css.nextButton} onClick={nextSlide}>
-            {/* &#8594; */}
-            <FaArrowRight />
-          </button>
-          {/* fa FaArrowLeft FaArrowRight */}
+          </Swiper>
         </div>
+
         <LinkButton
           name="primary"
           href="https://t.me/zatyshna_support"
