1eeec1a4440ac18dab003ce372caae5d71253aa6
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2025-02-10 19:27:07 +0200

    update styles

--- src/app/lessons/page.tsx
+++ src/app/lessons/page.tsx
@@ -1,6 +1,5 @@
 import Lessons from "@/components/Sections/Lessons";
 import Container from "@/components/Container";
-import { Icon } from "@iconify/react/dist/iconify.js";
 import css from "../page.module.scss";
 
 export default function LessonsPage() {
@@ -9,8 +8,7 @@ export default function LessonsPage() {
       <Container>
         <section className={css.intro}>
           <h2 className={css.pageTitle}>
-            <Icon icon="mdi:lightbulb-on-20" className={css.icon} /> Вчитися –
-            це легко, коли навчання побудоване під вас
+            Вчитися – це легко, коли навчання побудоване під вас
           </h2>
           <p className={css.pageSubtitle}>
             Вивчайте англійську комфортно та ефективно
